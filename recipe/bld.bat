9f619a208623623d510cc1655505074b30b7d63b
Sebastian Ehlert <sehlert@microsoft.com>
2024-09-15 12:06:48 +0200

    Update install location on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 @echo on
 setlocal enabledelayedexpansion
-
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\lib\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig
 set "MESON_RSP_THRESHOLD=320000"
 
-meson setup _build %MESON_ARGS% -Dlapack=custom -Dcustom_libraries='' -Dfortran_link_args='-llapack -lblas'
+meson setup _build -Dprefix=%LIBRARY_PREFIX% -Dbuildtype=release -Dlapack=custom -Dcustom_libraries='' -Dfortran_link_args='-llapack -lblas'
 if %ERRORLEVEL% neq 0 exit 1
 
 meson compile -C _build
