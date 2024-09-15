0767f06a71dc6dfb2f1365a12f5637b63cdfe65b
Sebastian Ehlert <sehlert@microsoft.com>
2024-09-15 11:14:39 +0200

    Update to version 3.7.0

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+@echo on
+setlocal enabledelayedexpansion
+
+set "MESON_RSP_THRESHOLD=320000"
+
+meson setup _build %MESON_ARGS% -Dlapack=custom -Dcustom_libraries=lapack,blas
+if %ERRORLEVEL% neq 0 exit 1
+
+meson compile -C _build
+if %ERRORLEVEL% neq 0 exit 1
+
+meson test -C _build --no-rebuild --print-errorlogs
+if %ERRORLEVEL% neq 0 exit 1
+
+meson install -C _build --no-rebuild
+if %ERRORLEVEL% neq 0 exit 1
